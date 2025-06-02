b582c61feee029c443f65949369f8f8e2cd6a197
charanraj-thiyagarajan-cdw-com <charanraj.thiyagarajan+cdwcorp@cdw.com>
2025-06-02 23:10:14 +0530

    token removed

--- app/core/config.py
+++ app/core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     AVIGILON_USER_KEY: str = ""
     AVIGILON_API_VERIFY_SSL: bool = False
     LOG_LEVEL: str = "INFO"
-    SESSION_TOKEN: str = "AXUAiv8KEDwecCpYnExqpbLKs4o-P0QSFgoUS4CNOy9RF7dYdvIMsXaVkq-Ru78aECe2E_nFdEBHsgncDncwWGUiEOaKtVSmk0AYgDhlGoU4ZZgqBmNoYXJhbjIdGhD15XlQda5BXYa0iielLzcHKglkZXZlbG9wZXI"
+    SESSION_TOKEN: str = ""
     class Config:
         env_file = ".env"
 
